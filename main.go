4efb3d9e1dcff0a448f50f2f8ade9bbcfb7e284c
NeginSal <neginsaljooghi@gmail.com>
2025-04-18 16:14:30 +0330

    add loadTasks function

--- main.go
+++ main.go
@@ -4,13 +4,6 @@ import (
 	"fmt"
 )
 
-type Task struct {
-	ID          int    `json:"id"`
-	title       string `json:"title"`
-	description string `json:"description"`
-	status      bool   `json:"status"`
-}
-
 func main() {
 	fmt.Println("hello world")
 }
---
+++ task.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Task struct {
+	ID          int    `json:"id"`
+	title       string `json:"title"`
+	description string `json:"description"`
+	status      bool   `json:"status"`
+}
+
+func loadTasks() ([]Task, error) {
+	var tasks []Task
+
+	file, err := os.ReadFile("tasks.json")
+	if err != nil {
+		return tasks, nil
+	}
+
+	err = json.Unmarshal(file, &tasks)
+	
+	return tasks, err
+}
